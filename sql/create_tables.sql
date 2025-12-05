f76ed2f8137d6b13bc70e02508d2bde849a1b6be
Siddhant000 <63959700+Siddhant000@users.noreply.github.com>
2025-12-05 15:33:09 +0530

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,24 @@
+CREATE OR REPLACE TABLE sales_raw (
+    order_id NUMBER,
+    order_date DATE,
+    customer_id NUMBER,
+    product_id NUMBER,
+    quantity NUMBER,
+    unit_price NUMBER
+);
 
+
+CREATE OR REPLACE TABLE customers_raw (
+    customer_id NUMBER,
+    customer_name STRING,
+    city STRING,
+    state STRING,
+    country STRING
+);
+
+
+CREATE OR REPLACE TABLE products_raw (
+    product_id NUMBER,
+    product_name STRING,
+    category STRING
+);
